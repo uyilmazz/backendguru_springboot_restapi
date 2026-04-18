006b92473d8c99bfc1c9b297b8742de3306af90d
uyilmazz <unal.yilmaz580@gmail.com>
2026-04-18 20:37:03 +0300

    .

--- src/test/java/com/example/restapiegitimders1/service/ProductServiceTest.java
+++ src/test/java/com/example/restapiegitimders1/service/ProductServiceTest.java
@@ -25,7 +25,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ProductService Unit Tests")
 public class ProductServiceTest {
